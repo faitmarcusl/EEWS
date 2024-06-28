b15bfc9494f8cd914e5f48d5e877044b69efd53c
Keno-00 <kenojose00@gmail.com>
2024-06-28 17:22:17 +0800

    REVISION: Multiclass Returns
    - the values returned from pred function are now appended list
    - unpack the list at the server script side
    - after unpacking, each item in the list still follows the old format

--- ai.py
+++ ai.py
@@ -14,20 +14,47 @@ if gpu_accel:
 model = YOLO(model_path,task='detect') #pytorch weights almost 30 fps
 
 
+#def pred(img):
+#    results = model.predict(img,verbose=print_verbose_info,device=0,show=False,save=False)
+#    result = results[0]
+#    box = result.boxes
+#    # capture screen and pass it thru mode 
+#    if len(box) > 0:                 # if some detection
+#        n = len(box) 
+#        for i in range(n):
+#            n = i
+#            conf = box.conf[n].item()
+#            #print(rl   
+#            if conf > minimum_confidence :           # confidence is greater than threshold
+#                coords = box.xyxy[n].tolist()
+#                coords = [round(x) for x in coords]
+#                cls = result.names[box.cls[n].item()]
+#                xmin,ymin,xmax,ymax = coords[0],coords[1],coords[2],coords[3]
+#                return xmin,ymin,xmax,ymax,cls
+            
+
 def pred(img):
-    results = model.predict(img,verbose=print_verbose_info,device=0,show=False,save=False)
+    # Perform prediction using the model
+    results = model.predict(img, verbose=print_verbose_info, device=0, show=False, save=False)
     result = results[0]
     box = result.boxes
-    # capture screen and pass it thru mode 
-    if len(box) > 0:                 # if some detection
-        n = len(box) 
-        for i in range(n):
-            n = i
-            conf = box.conf[n].item()
-            #print(rl   
-            if conf > minimum_confidence :           # confidence is greater than threshold
-                coords = box.xyxy[n].tolist()
+    
+    detections = []
+    
+    # Check if any detections are found
+    if len(box) > 0:
+        for i in range(len(box)):
+            conf = box.conf[i].item()
+            
+            # If confidence is greater than the minimum threshold
+            if conf > minimum_confidence:
+                coords = box.xyxy[i].tolist()
                 coords = [round(x) for x in coords]
-                cls = result.names[box.cls[n].item()]
-                xmin,ymin,xmax,ymax = coords[0],coords[1],coords[2],coords[3]
-                return xmin,ymin,xmax,ymax,cls
+                cls = result.names[box.cls[i].item()]
+                xmin, ymin, xmax, ymax = coords[0], coords[1], coords[2], coords[3]
+                
+                # Append the detected object's details to the list
+                detections.append((xmin, ymin, xmax, ymax, cls))
+                
+    # Return the list of detections
+    return detections
